00cc978f5a25ad4291d40eaf0fb5940347c04821
Aarushi <aarushisharda@gmail.com>
2018-04-23 19:59:08 +0530

    added date and list reversed

--- js/retain.js
+++ js/retain.js
@@ -20,13 +20,14 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: Date.now()
             });
             view.render();
         },
 
         getNotes: function() {
-            return model.getAllNotes();
+            return model.getAllNotes().reverse();
         },
 
         init: function() {
@@ -51,7 +52,8 @@ $(function(){
         render: function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
+                htmlStr += '<li class="note">'+'<span class="note-date">' +
+                        new Date(note.date).toString() + '</span>' +
                         note.content +
                     '</li>';
             });
